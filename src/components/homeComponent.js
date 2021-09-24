016164713f5261adad33e53ad22fe8b348b39994
abhishek427 <abhishek427821@gmail.com>
2021-09-24 15:09:43 +0530

    add react scroll

--- src/components/homeComponent.js
+++ src/components/homeComponent.js
@@ -1,28 +1,47 @@
-import * as React from 'react';
-import PropTypes from 'prop-types';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import CssBaseline from '@mui/material/CssBaseline';
-import useScrollTrigger from '@mui/material/useScrollTrigger';
-import Box from '@mui/material/Box';
-import Container from '@mui/material/Container';
-import Slide from '@mui/material/Slide';
-import Paper from '@mui/material/Paper';
-import About from './about';
-import Know from './knowmore';
-import Querry from './querry';
-import Profile from './founderProfile';
-import Footer from './footer';
-import Logo from "../assets/logos.png"
-import CardMedia from '@mui/material/CardMedia';
+import * as React from "react";
+import PropTypes from "prop-types";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import CssBaseline from "@mui/material/CssBaseline";
+import useScrollTrigger from "@mui/material/useScrollTrigger";
+import Box from "@mui/material/Box";
+import Container from "@mui/material/Container";
+import Slide from "@mui/material/Slide";
+import Paper from "@mui/material/Paper";
+import About from "./about";
+import Know from "./knowmore";
+import Querry from "./querry";
+import Profile from "./founderProfile";
+import Footer from "./footer";
+import Logo from "../assets/logos.png";
+import CardMedia from "@mui/material/CardMedia";
+import { makeStyles } from "@material-ui/core/styles";
+import { Link } from "react-scroll";
+import { Button } from "@mui/material";
 
 const styles = {
   media: {
-     height: 0,
-     paddingTop: '47%' // 16:9
-  }
-}
+    height: 0,
+    paddingTop: "47%", // 16:9
+  },
+};
+const useStyles = makeStyles((theme) => ({
+  sectionDesktop: {
+    display: "none",
+    [theme.breakpoints.up("md")]: {
+      display: "flex",
+      height: "100%",
+    },
+  },
+  sectionMobile: {
+    display: "flex",
+    height: "100%",
+    [theme.breakpoints.up("md")]: {
+      display: "none",
+    },
+  },
+}));
 
 function HideOnScroll(props) {
   const { children, window } = props;
@@ -50,28 +69,40 @@ HideOnScroll.propTypes = {
 };
 
 export default function HideAppBar(props) {
+  const classes = useStyles();
+
   return (
     <React.Fragment>
       <CssBaseline />
       <HideOnScroll {...props}>
-        <AppBar style={{backgroundColor:"#CEE5D0"}}>
-        
-      
-          <Toolbar>
-          <img src={Logo} alt="Girl in a jacket" width="150" height="60"/>
-            {/* <Typography variant="h6" component="div">
+        <AppBar style={{ backgroundColor: "#CEE5D0" }}>
+          <div className={classes.sectionDesktop}>
+            <Toolbar>
+              <img src={Logo} alt="Girl in a jacket" width="150" height="60" />
+              {/* <Typography variant="h6" component="div">
               Scroll to Hide App Bar
             </Typography> */}
-          </Toolbar>
+              <Link to={"aboutMe"} smooth={true} duration={500} activeClass="active" isDynamic={true}>
+                <Button>abhishek</Button>
+              </Link>
+            </Toolbar>
+          </div>
+          <div className={classes.sectionMobile}>
+            <Toolbar>
+              <Link to={"aboutMe"} smooth={true} duration={500} activeClass="active" isDynamic={true}>
+                <Button>abhishek</Button>
+              </Link>
+            </Toolbar>
+          </div>
         </AppBar>
       </HideOnScroll>
       <Toolbar />
       {/* <Container> */}
-          <About style={{marginTop: "50px"}}/>
-          <Know/>
-          <Querry/>
-          <Profile/>
-          <Footer/>
+      <About style={{ marginTop: "50px" }} />
+      <Know />
+      <Querry />
+      <Profile />
+      <Footer />
       {/* <Paper style={{backgroundColor:"black"}} elevation={3} />
         <Box sx={{ my: 2 }}>
         <Paper style={{backgroundColor:"black"}} elevation={3} />
